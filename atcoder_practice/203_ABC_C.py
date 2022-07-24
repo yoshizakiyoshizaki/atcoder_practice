2c015df5720f0fdb82a86652c36046118667900d
yoshizakiyoshizaki <17487143+yoshizakiyoshizaki@users.noreply.github.com>
2022-07-25 04:02:06 +0900

    added

---
+++ atcoder_practice/203_ABC_C.py
@@ -0,0 +1,25 @@
+
+
+
+# mura = 0
+
+
+# for _ in range(1000):
+#     print("村", mura, "所持金", K)
+#     for num in range(N):
+#         # A村、B円渡す
+#         A, B = map(int, input().split())
+#         #村0, 所持金0
+#         # 友達がいる場合
+#         if A == mura:
+#             K += B
+#         else:
+#             pass
+
+#     mura += 1
+#     K -= 1
+
+#     if K == 0:
+#         break
+
+# print(mura)
