b7b59c22630fa3ed8f7fcf858728b68354870e33
yoshizakiyoshizaki <17487143+yoshizakiyoshizaki@users.noreply.github.com>
2022-08-07 04:02:51 +0900

    added

---
+++ atcoder_practice/182_ABC_C.py
@@ -0,0 +1,4 @@
+N = int(input())
+
+str_N = str(N)
+print(list[str_N])
---
+++ atcoder_practice/183_ABC_C.py
@@ -0,0 +1,7 @@
+N, K = map(int, input().split())
+
+time = []
+
+for _ in range(N):
+    T = list(map(int, input().split()))
+    time.append(T)
